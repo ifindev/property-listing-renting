d2d21ab6f334f3b81baaaef955817d8248cb1564
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-02 20:32:38 +0700

    move SideNavbar to new component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,6 @@
 import React, { useState } from 'react';
-import {
-  ChakraProvider,
-  Flex,
-  Text,
-  List,
-  ListItem,
-  theme,
-} from '@chakra-ui/react';
-import {
-  MdViewQuilt,
-  MdLocationCity,
-  MdDateRange,
-  MdLibraryBooks,
-  MdEmail,
-} from 'react-icons/md';
+import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
+import { SideNavbar } from './components';
 
 function App() {
   const [selectedMenu, setSelectedMenu] = useState('Listings');
@@ -45,62 +32,5 @@ function App() {
     </ChakraProvider>
   );
 }
-
-const SideNavbar = ({ menus, selectedMenu, handleSelectedMenu }) => {
-  return (
-    <Flex
-      flexDir="column"
-      justifyContent="space-between"
-      alignItems="center"
-      py={10}
-      w={20}
-      bg="white"
-      boxShadow="xl"
-    >
-      <List
-        d="flex"
-        flexDir="column"
-        alignItems="center"
-        spacing={10}
-        w="100%"
-        _hover={{
-          cursor: 'pointer',
-        }}
-      >
-        {menus.map(menu => {
-          return (
-            <ListItem
-              d="flex"
-              flexDir="column"
-              alignItems="center"
-              fontSize="xs"
-              w="100%"
-              color={`${selectedMenu === menu ? 'purple.600' : 'gray.500'}`}
-              borderColor={`${selectedMenu === menu ? 'purple.600' : ''}`}
-              borderLeft={`${selectedMenu === menu ? '4px' : ''}`}
-              onClick={() => handleSelectedMenu(menu)}
-            >
-              <Text fontSize={30}>
-                {menu === 'Dashboard' ? (
-                  <MdViewQuilt />
-                ) : menu === 'Listings' ? (
-                  <MdLocationCity />
-                ) : menu === 'Bookings' ? (
-                  <MdDateRange />
-                ) : menu === 'Tasks' ? (
-                  <MdLibraryBooks />
-                ) : (
-                  <MdEmail />
-                )}
-              </Text>
-              <Text>{menu}</Text>
-            </ListItem>
-          );
-        })}
-      </List>
-      <div>Hello</div>
-    </Flex>
-  );
-};
 
 export default App;
---
+++ src/components/SideNavbar.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Flex, Text, List, ListItem } from '@chakra-ui/react';
+import {
+  MdViewQuilt,
+  MdLocationCity,
+  MdDateRange,
+  MdLibraryBooks,
+  MdEmail,
+} from 'react-icons/md';
+
+const SideNavbar = ({ menus, selectedMenu, handleSelectedMenu }) => {
+  return (
+    <Flex
+      flexDir="column"
+      justifyContent="space-between"
+      alignItems="center"
+      py={10}
+      w={20}
+      bg="white"
+      boxShadow="xl"
+    >
+      <List
+        d="flex"
+        flexDir="column"
+        alignItems="center"
+        spacing={10}
+        w="100%"
+        _hover={{
+          cursor: 'pointer',
+        }}
+      >
+        {menus.map(menu => {
+          return (
+            <ListItem
+              d="flex"
+              flexDir="column"
+              alignItems="center"
+              fontSize="xs"
+              w="100%"
+              color={`${selectedMenu === menu ? 'purple.600' : 'gray.500'}`}
+              borderColor={`${selectedMenu === menu ? 'purple.600' : ''}`}
+              borderLeft={`${selectedMenu === menu ? '4px' : ''}`}
+              onClick={() => handleSelectedMenu(menu)}
+            >
+              <Text fontSize={30}>
+                {menu === 'Dashboard' ? (
+                  <MdViewQuilt />
+                ) : menu === 'Listings' ? (
+                  <MdLocationCity />
+                ) : menu === 'Bookings' ? (
+                  <MdDateRange />
+                ) : menu === 'Tasks' ? (
+                  <MdLibraryBooks />
+                ) : (
+                  <MdEmail />
+                )}
+              </Text>
+              <Text>{menu}</Text>
+            </ListItem>
+          );
+        })}
+      </List>
+      <div>Hello</div>
+    </Flex>
+  );
+};
+
+export default SideNavbar;
