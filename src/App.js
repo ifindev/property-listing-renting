7b0946d46103827596f34b453e03fe0a4e107ffd
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-02 20:37:13 +0700

    Move everything from App to UserDashboard page component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,11 @@
-import React, { useState } from 'react';
-import { ChakraProvider, Flex, theme } from '@chakra-ui/react';
-import { SideNavbar } from './components';
+import React from 'react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
+import { UserDashboard } from './pages';
 
 function App() {
-  const [selectedMenu, setSelectedMenu] = useState('Listings');
-  const menus = ['Dashboard', 'Listings', 'Bookings', 'Tasks', 'Inbox'];
   return (
     <ChakraProvider theme={theme}>
-      <Flex style={{ height: '100vh' }} bg="gray.50">
-        <SideNavbar
-          menus={menus}
-          selectedMenu={selectedMenu}
-          handleSelectedMenu={setSelectedMenu}
-        />
-        <Flex w="100%">
-          <div className="flex-auto bg-gray-100 p-8">
-            {selectedMenu === 'Dashboard' ? (
-              <p>Dashboard</p>
-            ) : selectedMenu === 'Listings' ? (
-              <p>Listings</p>
-            ) : selectedMenu === 'Bookings' ? (
-              <p>Bookings</p>
-            ) : selectedMenu === 'Tasks' ? (
-              <p>Tasks</p>
-            ) : (
-              <p>Inbox</p>
-            )}
-          </div>
-        </Flex>
-      </Flex>
+      <UserDashboard />
     </ChakraProvider>
   );
 }
---
+++ src/pages/UserDashboard.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import { Flex } from '@chakra-ui/react';
+import { SideNavbar } from '../components';
+
+const UserDashboard = () => {
+  const [selectedMenu, setSelectedMenu] = useState('Listings');
+  const menus = ['Dashboard', 'Listings', 'Bookings', 'Tasks', 'Inbox'];
+  return (
+    <div>
+      <Flex style={{ height: '100vh' }} bg="gray.50">
+        <SideNavbar
+          menus={menus}
+          selectedMenu={selectedMenu}
+          handleSelectedMenu={setSelectedMenu}
+        />
+        <Flex w="100%">
+          <div className="flex-auto bg-gray-100 p-8">
+            {selectedMenu === 'Dashboard' ? (
+              <p>Dashboard</p>
+            ) : selectedMenu === 'Listings' ? (
+              <p>Listings</p>
+            ) : selectedMenu === 'Bookings' ? (
+              <p>Bookings</p>
+            ) : selectedMenu === 'Tasks' ? (
+              <p>Tasks</p>
+            ) : (
+              <p>Inbox</p>
+            )}
+          </div>
+        </Flex>
+      </Flex>
+    </div>
+  );
+};
+
+export default UserDashboard;
