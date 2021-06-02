daee892e45e7d3cb8c009a13a25cf06a9959406a
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-02 22:02:46 +0700

    Add SearchField component in Listings

--- src/pages/HostMainPage.jsx
+++ src/pages/HostMainPage.jsx
@@ -3,35 +3,33 @@ import { Flex } from '@chakra-ui/react';
 import { SideNavbar } from '../components';
 import Listings from './Listings';
 
-const UserDashboard = () => {
+const HostMainPage = () => {
   const [selectedMenu, setSelectedMenu] = useState('Listings');
   const menus = ['Dashboard', 'Listings', 'Bookings', 'Tasks', 'Inbox'];
   return (
     <div>
-      <Flex style={{ height: '100vh' }} bg="gray.50">
+      <Flex style={{ height: '100vh', overflow: 'hidden' }} bg="gray.50">
         <SideNavbar
           menus={menus}
           selectedMenu={selectedMenu}
           handleSelectedMenu={setSelectedMenu}
         />
         <Flex w="100%">
-          <div className="flex-auto bg-gray-100 p-8">
-            {selectedMenu === 'Dashboard' ? (
-              <p>Dashboard</p>
-            ) : selectedMenu === 'Listings' ? (
-              <Listings />
-            ) : selectedMenu === 'Bookings' ? (
-              <p>Bookings</p>
-            ) : selectedMenu === 'Tasks' ? (
-              <p>Tasks</p>
-            ) : (
-              <p>Inbox</p>
-            )}
-          </div>
+          {selectedMenu === 'Dashboard' ? (
+            <p>Dashboard</p>
+          ) : selectedMenu === 'Listings' ? (
+            <Listings />
+          ) : selectedMenu === 'Bookings' ? (
+            <p>Bookings</p>
+          ) : selectedMenu === 'Tasks' ? (
+            <p>Tasks</p>
+          ) : (
+            <p>Inbox</p>
+          )}
         </Flex>
       </Flex>
     </div>
   );
 };
 
-export default UserDashboard;
+export default HostMainPage;
--- src/pages/Listings.jsx
+++ src/pages/Listings.jsx
@@ -1,9 +1,96 @@
 import React from 'react';
+import {
+  Box,
+  Flex,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  Text,
+  Button,
+} from '@chakra-ui/react';
+import { MdSearch, MdAddCircleOutline } from 'react-icons/md';
 
 const Listings = () => {
   return (
+    <Flex pt={10} pb={5} px={5} height="100%" w="100%">
+      <Flex
+        px={5}
+        w={350}
+        flexDir="column"
+        overflow="scroll"
+        sx={{
+          '&::-webkit-scrollbar': {
+            display: 'none',
+          },
+          '&::-webkit-scrollbar-thumb': {
+            display: 'none',
+          },
+        }}
+      >
+        <SearchField />
+        <Box mt={8}>
+          <Flex justifyContent="space-between" alignItems="center" px={1}>
+            <Text fontSize="2xl" color="gray.700">
+              <strong>My Listings</strong>
+            </Text>
+            <Button
+              bg="purple.600"
+              color="white"
+              fontSize={25}
+              sx={{
+                '&:hover, &:active': {
+                  backgroundColor: 'purple.500',
+                },
+              }}
+              _focus={{
+                outline: 'none',
+              }}
+            >
+              +
+            </Button>
+          </Flex>
+        </Box>
+      </Flex>
+      <Flex
+        ml={10}
+        p={10}
+        w="100%"
+        bg="whiteAlpha.900"
+        boxShadow="md"
+        borderRadius={10}
+      >
+        Listing Details
+      </Flex>
+    </Flex>
+  );
+};
+
+const SearchField = () => {
+  return (
     <div>
-      <h2>Listings</h2>
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents="none"
+          children={
+            <Text fontSize={20} color="gray.400">
+              <MdSearch />
+            </Text>
+          }
+        />
+        <Input
+          type="search"
+          placeholder="Search"
+          boxShadow="sm"
+          backgroundColor="white"
+          borderWidth={2}
+          _hover={{
+            borderColor: 'purple.300',
+          }}
+          _focus={{
+            borderColor: 'purple.600',
+          }}
+        />
+      </InputGroup>
     </div>
   );
 };
