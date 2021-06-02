3a47b9f8632235e62e79fb1a11da2ef29165ef60
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-02 18:17:33 +0700

    Remove unnecessary files and add SideNavbar component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   ChakraProvider,
-  Box,
+  Flex,
   Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
+  List,
+  ListItem,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+import {
+  MdViewQuilt,
+  MdLocationCity,
+  MdDateRange,
+  MdLibraryBooks,
+  MdEmail,
+} from 'react-icons/md';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <Flex style={{ height: '100vh' }} bg="gray.50">
+        <SideNavbar />
+        <Flex w="100%">Hello</Flex>
+      </Flex>
     </ChakraProvider>
   );
 }
+
+const SideNavbar = () => {
+  const [selectedMenu, setSelectedMenu] = useState('Dashboard');
+  const menus = ['Dashboard', 'Listings', 'Bookings', 'Tasks', 'Inbox'];
+  return (
+    <Flex
+      flexDir="column"
+      justifyContent="space-between"
+      alignItems="center"
+      py={10}
+      w={20}
+      bg="white"
+      boxShadow="xl"
+    >
+      <List
+        d="flex"
+        flexDir="column"
+        alignItems="center"
+        spacing={10}
+        w="100%"
+        _hover={{
+          cursor: 'pointer',
+        }}
+      >
+        {menus.map(menu => {
+          return (
+            <ListItem
+              d="flex"
+              flexDir="column"
+              alignItems="center"
+              fontSize="xs"
+              w="100%"
+              color={`${selectedMenu === menu ? 'purple.600' : 'gray.500'}`}
+              borderColor={`${selectedMenu === menu ? 'purple.600' : ''}`}
+              borderLeft={`${selectedMenu === menu ? '4px' : ''}`}
+              onClick={() => setSelectedMenu(menu)}
+            >
+              <Text fontSize={30}>
+                {menu === 'Dashboard' ? (
+                  <MdViewQuilt />
+                ) : menu === 'Listings' ? (
+                  <MdLocationCity />
+                ) : menu === 'Bookings' ? (
+                  <MdDateRange />
+                ) : menu === 'Tasks' ? (
+                  <MdLibraryBooks />
+                ) : (
+                  <MdEmail />
+                )}
+              </Text>
+              <Text>{menu}</Text>
+            </ListItem>
+          );
+        })}
+      </List>
+      <div>Hello</div>
+    </Flex>
+  );
+};
 
 export default App;
