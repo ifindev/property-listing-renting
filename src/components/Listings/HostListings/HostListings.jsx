03beb1820379168245e0bf6037c80bbdf23571a7
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-03 15:14:24 +0700

    Move ListingCards to its own file component

--- src/components/Listings/HostListings/HostListings.jsx
+++ src/components/Listings/HostListings/HostListings.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { Flex, Box, Image, Text, Badge } from '@chakra-ui/react';
+import { Flex, Box } from '@chakra-ui/react';
 
 import AddListing from './AddListing';
 import FilterListings from './FilterListings';
 import SearchField from './SearchField';
+import ListingCards from './ListingCards';
 
 const listings = require('../../../data/listings');
 
@@ -46,88 +47,6 @@ const HostListings = () => {
         />
       </Flex>
     </Flex>
-  );
-};
-
-const ListingCards = ({
-  listingsData,
-  selectedListing,
-  handleSelectedListing,
-}) => {
-  return (
-    <Box px={2}>
-      {listingsData.map(listing => {
-        return (
-          <Flex
-            key={listing.id}
-            p={0}
-            mt={10}
-            _first={{ marginTop: '0px' }}
-            _last={{ marginBottom: '620px' }}
-            _hover={{
-              cursor: 'pointer',
-            }}
-            borderRadius="xl"
-            bg="white"
-            boxShadow="md"
-            onClick={() => {
-              selectedListing = listing.id;
-              handleSelectedListing(selectedListing);
-            }}
-            borderWidth={`${selectedListing === listing.id ? '2px' : '0px'}`}
-            borderColor={`${selectedListing === listing.id ? 'blue.400' : ''}`}
-          >
-            <Box w="250px">
-              <Image
-                src={listing.image}
-                borderTopLeftRadius="lg"
-                borderBottomLeftRadius="lg"
-                boxSize="150px"
-                objectFit="cover"
-              />
-            </Box>
-            <Flex flexDir="column" w="100%" p={3}>
-              <Text
-                fontWeight="bold"
-                fontSize={19}
-                color={`${
-                  selectedListing === listing.id ? 'blue.500' : 'blue.700'
-                }`}
-              >
-                {listing.title}
-              </Text>
-              <Text color="gray.500" mt={2}>
-                {listing.location}
-              </Text>
-              <Badge
-                d="flex"
-                justifyContent="center"
-                w="70px"
-                py={1}
-                mt={2}
-                borderRadius="lg"
-                bg={`${
-                  listing.status === 'Booked'
-                    ? 'green.100'
-                    : listing.status === 'Vacant'
-                    ? 'red.100'
-                    : 'yellow.100'
-                }`}
-                color={`${
-                  listing.status === 'Booked'
-                    ? 'green.600'
-                    : listing.status === 'Vacant'
-                    ? 'red.600'
-                    : 'yellow.600'
-                }`}
-              >
-                {listing.status}
-              </Badge>
-            </Flex>
-          </Flex>
-        );
-      })}
-    </Box>
   );
 };
 
