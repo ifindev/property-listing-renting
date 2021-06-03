fa26aa38188dc529ec6d51793b3c3ce2c3ad8d85
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-03 14:34:50 +0700

    Move listings data to new file and initialize in HostListings component

--- src/components/Listings/HostListings/HostListings.jsx
+++ src/components/Listings/HostListings/HostListings.jsx
@@ -5,7 +5,11 @@ import AddListing from './AddListing';
 import FilterListings from './FilterListings';
 import SearchField from './SearchField';
 
+const listings = require('../../../data/listings');
+
 const HostListings = () => {
+  const listingsData = listings;
+
   return (
     <Flex px={3} w={500} flexDir="column">
       <Box>
@@ -26,58 +30,13 @@ const HostListings = () => {
           },
         }}
       >
-        <ListingCards />
+        <ListingCards listingsData={listingsData} />
       </Flex>
     </Flex>
   );
 };
 
-const ListingCards = () => {
-  const listingsData = [
-    {
-      title: 'Manhattan cozy',
-      location: 'Manhattan, New York',
-      status: 'Booked',
-      image:
-        'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
-    },
-    {
-      title: 'Minimalistic clean and white ',
-      location: 'Manhattan, New York',
-      status: 'Booked',
-      image:
-        'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
-    },
-    {
-      title: "The Artist's Studio",
-      location: "Hell's Kitchen, New York",
-      status: 'Vacant',
-      image:
-        'https://images.unsplash.com/photo-1486304873000-235643847519?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
-    },
-    {
-      title: 'Central Bright Studio',
-      location: 'Manhattan, New York',
-      status: 'Pending',
-      image:
-        'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
-    },
-    {
-      title: 'Cozy Living Studio',
-      location: '	Hempstead, New York',
-      status: 'Vacant',
-      image:
-        'https://images.unsplash.com/photo-1556593825-c11de986cb0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
-    },
-    {
-      title: 'Cozy Family Room',
-      location: '	Syracause, New York',
-      status: 'Pending',
-      image:
-        'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
-    },
-  ];
-
+const ListingCards = ({ listingsData }) => {
   return (
     <Box px={2}>
       {listingsData.map(listing => {
