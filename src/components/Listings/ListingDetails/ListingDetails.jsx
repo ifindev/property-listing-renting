683497c270bc736c2409fdcf68c6a3c7ee87352b
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-03 10:48:44 +0700

    Move listingDetails to new component

---
+++ src/components/Listings/ListingDetails/ListingDetails.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+
+const ListingDetails = () => {
+  return (
+    <Flex
+      ml={10}
+      p={10}
+      w="100%"
+      bg="whiteAlpha.900"
+      boxShadow="md"
+      borderRadius={10}
+    >
+      Listing Details
+    </Flex>
+  );
+};
+
+export default ListingDetails;
