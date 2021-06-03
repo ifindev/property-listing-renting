829f1375cded0bd8f04ee83eaf21ea1b10d5aa65
ifindev <arifin.effendi@mail.ugm.ac.id>
2021-06-03 14:40:12 +0700

    Add ids to listings data

--- src/data/listings.js
+++ src/data/listings.js
@@ -1,5 +1,6 @@
 const listings = [
   {
+    id: 1,
     title: 'Manhattan cozy',
     location: 'Manhattan, New York',
     status: 'Booked',
@@ -7,6 +8,7 @@ const listings = [
       'https://images.unsplash.com/photo-1522771739844-6a9f6d5f14af?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
   },
   {
+    id: 2,
     title: 'Minimalistic clean and white ',
     location: 'Manhattan, New York',
     status: 'Booked',
@@ -14,6 +16,7 @@ const listings = [
       'https://images.unsplash.com/photo-1554995207-c18c203602cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80',
   },
   {
+    id: 3,
     title: "The Artist's Studio",
     location: "Hell's Kitchen, New York",
     status: 'Vacant',
@@ -21,6 +24,7 @@ const listings = [
       'https://images.unsplash.com/photo-1486304873000-235643847519?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1489&q=80',
   },
   {
+    id: 4,
     title: 'Central Bright Studio',
     location: 'Manhattan, New York',
     status: 'Pending',
@@ -28,6 +32,7 @@ const listings = [
       'https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
   },
   {
+    id: 5,
     title: 'Cozy Living Studio',
     location: '	Hempstead, New York',
     status: 'Vacant',
@@ -35,6 +40,7 @@ const listings = [
       'https://images.unsplash.com/photo-1556593825-c11de986cb0b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80',
   },
   {
+    id: 6,
     title: 'Cozy Family Room',
     location: '	Syracause, New York',
     status: 'Pending',
